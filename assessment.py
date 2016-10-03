02160a3a081028a0e3fa30617c2201e2fb36dfa9
vagrant <vagrant@vagrant.vm>
2016-10-03 02:19:54 +0000

    part 3.2 done

--- assessment.py
+++ assessment.py
@@ -83,10 +83,16 @@ def hometown_greeting(town_name, first, last):
 # 1. Write a function ``increment()`` with a nested inner function, ``add()`` 
 #    inside of it. The outer function should take ``x``, an integer which
 #    defaults to 1. The inner function should take ``y`` and add ``x`` and ``y`` together.
-
+def increment(x=1):
+    def add(y):
+        """Returns sum of two numbers."""
+        return x+y
+    return add
 # 2. Call the function ``increment()`` with x = 5. Assign what is returned to a variable name, addfive. Call 
 #    addfive with y = 5. Call again with y = 20.
-
+addfive = increment(5)
+print addfive(5)
+print addfive(20)
 # 3. Make a function that takes in a number and a list of numbers. It should append
 #    the number to the list of numbers and return the list.
 
